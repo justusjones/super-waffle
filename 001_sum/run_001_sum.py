bf61cb806c4eab52da5f51a4aa73a5366544a214
Jeremy Jones <jemejones@gmail.com>
2019-11-15 07:17:18 -0500

    adding first project

---
+++ 001_sum/run_001_sum.py
@@ -0,0 +1,7 @@
+def calculate_sum(number_list):
+    """return the calculated sum of a list of numbers
+
+    Arguments:
+    number_list -- a list of numbers (ints, floats)
+    """
+    return sum(number_list)
